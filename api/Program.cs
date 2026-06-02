d3be4c8d59314024a9752a8d82952328db2b6b2c
Praga <c.saravanaraja@gmail.com>
2026-06-03 00:25:23 +0530

    Remove incorrect using statement

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,6 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MobileApp.Api.Models;
